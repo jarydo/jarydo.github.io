c14832800582fc3abf26ecfdd53ba46bb008b17e
jarydo <jarydnoahdiamond@gmail.com>
2025-06-29 23:42:11 -0400

    proxy config

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/">
       <Routes>
         <Route path="/" element={<PersonalPage />} />
         <Route path="/recruiter" element={<RecruiterPage />} />
